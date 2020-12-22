06b53897283ca7bcd291090fd608376505e057b8
peter-hunt <huangtnhao@outlook.com>
2020-12-22 16:43:55 +0800

    Improved project installing and documentation

--- setup.py
+++ setup.py
@@ -3,11 +3,21 @@ from setuptools import setup
 from __init__ import __version__
 
 
+with open('README.md') as file:
+    long_description = file.read()
+
+with open('requirements.txt') as file:
+    requirements = file.read().strip().split('\n')
+
+
 setup(
-    name='Cocktail Lang',
+    name='cocktail-lang',
     version=__version__,
-    description='Cocktail Programming Language',
     author='Peter Hunt',
     author_email='huangtianhao@icloud.com',
+    description='Cocktail Programming Language',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     python_requires='>=3.8',
+    setup_requires=requirements,
 )
